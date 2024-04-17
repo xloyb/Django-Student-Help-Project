69fea68962d641b5c6cd5172693a7c3d6ce6e278
mydevify <boussaadilouay9@gmail.com>
2024-04-17 22:09:01 +0100

    First commit

---
+++ Student_Help/Student_Help/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Student_Help project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Student_Help.settings')
+
+application = get_asgi_application()
