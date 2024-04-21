f043c1d57290b9a679811f42a392c44f161fb51e
dina <dinaaliyoussef22@gmail.com>
2024-04-21 14:10:35 +0100

    setting up the database

--- Student_Help/Student_Help/settings.py
+++ Student_Help/Student_Help/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,7 +95,7 @@ WSGI_APPLICATION = 'Student_Help.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
