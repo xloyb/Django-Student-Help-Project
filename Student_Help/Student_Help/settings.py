e2425506ff392217c1311b14c0507e08f5d75a29
mydevify <boussaadilouay9@gmail.com>
2024-04-21 15:01:40 +0100

    Revert "added LOGIN_REDIRECT_URL & LOGIN_URL"
    This reverts commit f37a1c344536ca8ee337eac46c8b4e8dbce2b5c7.

--- Student_Help/Student_Help/settings.py
+++ Student_Help/Student_Help/settings.py
@@ -144,6 +144,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
-
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGIN_URL = 'login'
