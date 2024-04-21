489f8f7bcaac689688c35ee3b70caf64178baca9
mydevify <boussaadilouay9@gmail.com>
2024-04-21 15:45:21 +0100

    created registration function.

--- Student_Help/core/views.py
+++ Student_Help/core/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def  home(request):
     return render (request, 'home.html')
 
 
-def  testDashboard(request):
+def  Dashboard(request):
     return render (request, 'dashboard.html')
 
 
@@ -15,8 +20,18 @@ def  layout(request):
 def  profile(request):
     return render (request, 'pages/profile.html')
 
-def  register(request):
-    return render (request, 'registration/register.html')
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Hi {username}, your account was created successfully')
+            return redirect('dashboard')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})
 
 def  login(request):
     return render (request, 'registration/login.html')
