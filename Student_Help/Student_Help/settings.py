7282b50cfde4b6e635b6bd9725c7c3e0620e2074
mydevify <boussaadilouay9@gmail.com>
2024-05-11 16:05:48 +0100

    updated Some Components.

--- Student_Help/Student_Help/settings.py
+++ Student_Help/Student_Help/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/core/static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
